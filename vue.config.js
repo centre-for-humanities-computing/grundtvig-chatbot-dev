dc59914617eb6f3957c5a073a834c86fffdd53f1
Max Eckardt <max@cas.au.dk>
2019-10-24 20:16:34 +0200

    removed fontawesome dependency

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/grundtvig-chatbot-vue/'
-    : '/'
-  outputDir: './docs/',
-  assetsDir: '../public/'
+  outputDir: './docs/'
 }
