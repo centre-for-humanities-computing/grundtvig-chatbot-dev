2e92aeb9b951997c23911882da930b4b4f23488a
Max Roald Eckardt <mr.eckardt@gmail.com>
2019-10-24 19:45:56 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/grundtvig-chatbot-vue/'
+    : '/'
   outputDir: './docs/',
+  assetsDir: '../public/'
 }
